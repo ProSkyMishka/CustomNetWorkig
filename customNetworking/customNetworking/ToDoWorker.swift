248aba7d2f51e0e2cfc59f81582825b8eacbc72d
ProSkyMishka <mprozorskiy@mail.ru>
2023-12-01 21:36:01 +0300

    Update

--- customNetworking/customNetworking/ToDoWorker.swift
+++ customNetworking/customNetworking/ToDoWorker.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+enum ToDoEndpoint: Endpoint {
+    case toDo(Int)
+    
+    var compositePath: String {
+        switch self {
+            
+        case .toDo(let number):
+            "/toDos/\(number)"
+        }
+    }
+    
+    var headers: HeaderModel { [:] }
+}
+
+struct ToDo: Codable {
+    let userID, id: Int
+    let title: String
+    let completed: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case id, title, completed
+    }
+}
+
+final class ToDoWorker {
+    private let networking: Networking = Networking(
+        baseUrl: "https://jsonplaceholder.typicode.com"
+    )
+    
+    typealias ToDoResult = (Result<ToDo, Error>) -> Void
+    
+    func fetchToDos(completion: @escaping ToDoResult) {
+        let endpoint = ToDoEndpoint.toDo(1)
+        let request = Request(endpoint: endpoint, method: .get)
+        networking.executeRequest(with: request) { result in
+            switch result {
+                
+            case .success(let model):
+                if let data = model.data {
+                    print(String(data: data, encoding: .utf8))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            
+        }
+    }
+}
