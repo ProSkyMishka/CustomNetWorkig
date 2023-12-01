576cc4db6183a3b2e7d658808e1752a4c2a8c3ee
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2023-12-01 20:51:00 +0300

    Delete CustomNetWorking directory

--- CustomNetWorking/CustomNetWorking/CustomNetworking/Aliases.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Aliases.swift
-//  CustomNetWorking
-//
-//  Created by Михаил Прозорский on 01.12.2023.
-//
-
-typealias RequestParameters = [(key: String, value: String)]
-typealias HeaderModel = [String: String]
